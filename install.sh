5f8935d5da6c3365fa55799eb85630a6566beac6
Jesus David García Gomez <1581288+sarvaje@users.noreply.github.com>
2025-02-20 16:16:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,14 +10,3 @@ mv ~/tmpzshrc ~/.zshrc
 
 # Install needed gems
 sudo gem install solargraph
-
-
-# Spin extension
-cat <<EOF > "/tmp/private-extensions.json"
-{
-  "github": {
-    "spin-vscode.vsix": "Shopify/spin-vscode"
-  }
-}
-EOF
-/usr/local/bin/install-vscode-private-extensions "$(cat /etc/spin/secrets/github_token_shopify)" "/tmp/private-extensions.json" "/home/spin/.cursor-server/extensions"
